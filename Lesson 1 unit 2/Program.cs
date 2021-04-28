ba9f348f5b5c7fd8e284f38fdc2a491c8e3c6768
Ivan <i.s.senkin@yandex.ru>
2021-04-28 19:17:57 +0300

    Добавьте файлы проекта.

---
+++ Lesson 1 unit 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_1_unit_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.Ввести вес и рост человека. Рассчитать и вывести индекс массы тела(ИМТ) по формуле I = m / (h * h); где m — масса тела в килограммах, h — рост в метрах.
+
+            Console.Write("Введите вес человека в килограммах: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите рост человека в метрах: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Индекс массы тела: {weight / (height * height):F3}");
+        }
+    }
+}
